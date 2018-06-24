5334580d12b4a8c11bbfb41118a5887370b715ef
amanjpro <amanj.sherwany@usi.ch>
2018-06-23 22:27:32 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "emr-log-viewer"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+
+libraryDependencies ++= Seq("com.amazonaws" % "aws-java-sdk" % "1.11.354")
+
+        
---
+++ src/main/scala/me/amanj/emr/log/viewer/io/S3Objects.scala
@@ -0,0 +1,25 @@
+package me.amanj.emr.log.viewer.io
+
+import java.io.{File, PrintWriter}
+
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+
+import scala.collection.JavaConverters._
+
+object S3Objects {
+  val s3Client = AmazonS3ClientBuilder.defaultClient()
+
+  def ls(key: String): Seq[String] = {
+    s3Client.listObjects(key).getObjectSummaries.asScala.map(a => a.getKey)
+  }
+
+  def download(bucket: String, key: String, dest: String): Unit = {
+    val objectStream = s3Client.getObject(bucket, key).getObjectContent
+    val printer = new PrintWriter(new File(dest))
+    scala.io.Source.fromInputStream(objectStream).foreach(ch =>
+      printer.append(ch)
+    )
+    objectStream.close()
+    printer.close()
+  }
+}
