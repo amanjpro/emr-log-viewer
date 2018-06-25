2b367b5df7cf9c94d458d7f9ec047f7e019e66dc
amanjpro <amanj.sherwany@usi.ch>
2018-06-24 23:35:28 -0400

    Parallelize downloading container/node/steps logs

--- src/main/scala/me/amanj/emr/log/viewer/Main.scala
+++ src/main/scala/me/amanj/emr/log/viewer/Main.scala
@@ -1,21 +1,41 @@
 package me.amanj.emr.log.viewer
 
 import java.io.File
+
 import config.Config
 import io.S3Objects
-
 import io.Implicits._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
 
 object Main {
 
   val config = new Config("bucket", "clustreId", "destination", 100)
 
+
   def main(args: Array[String]): Unit = {
-    S3Objects.ls(config.s3Bucket, config.clusterId / "node", config).foreach(downloadDirectory)
 
-    S3Objects.ls(config.s3Bucket, config.clusterId / "steps", config).foreach(downloadDirectory)
+    val nodeFuture = Future[Unit] {
+      S3Objects.ls(config.s3Bucket, config.clusterId / "node", config).foreach(downloadDirectory)
+    }
 
-    S3Objects.ls(config.s3Bucket, config.clusterId / "containers", config).foreach(downloadDirectory)
+    val stepsFuture = Future[Unit] {
+      S3Objects.ls(config.s3Bucket, config.clusterId / "steps", config).foreach(downloadDirectory)
+    }
+
+    val containersFuture = Future[Unit]{
+      S3Objects.ls(config.s3Bucket, config.clusterId / "containers", config).foreach(downloadDirectory)
+    }
+
+    // old school busy waiting, I believe this is safer than waiting for n time and hoping
+    // that the computation is already done
+    while(!containersFuture.isCompleted &&
+      !stepsFuture.isCompleted &&
+      !nodeFuture.isCompleted) {}
+
   }
 
   def downloadDirectory(key: String): Unit = {
